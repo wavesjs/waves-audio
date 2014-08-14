f98c6fe8b16361ab84d42bb26fd7ab22985d6564
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-08-14 16:16:26 +0200

    - modernized code

--- src/index.js
+++ src/index.js
@@ -5,26 +5,45 @@
  */
 "use strict";
 
+var Scheduled = require("../scheduler/scheduled");
+//var Transported = require("../transporter/transported");
+
+class ScheduledMetronome extends Scheduled {
+  reset(time) {
+    return time;
+  }
+
+  execute(time) {
+    return this.parent.trigger(time);
+  }
+}
+
 class Metronome {
-  constructor(name = "unnamed") {
-    this.period = 1 // in sec
+  constructor(name = "metronome") {
+    this.name = name;
+
+    this.period = 1; // in sec
     this.attack = 0.002;
     this.release = 0.098;
 
     this.__gainNode = audioContext.createGain();
 
     this.__envNode = audioContext.createGain();
+    this.__envNode.gain.value = 0.0;
     this.__envNode.connect(this.__gainNode);
 
     this.__osc = audioContext.createOscillator();
     this.__osc.frequency.value = 600;
     this.__osc.start(0);
     this.__osc.connect(this.__envNode);
+
+    this.scheduled = new ScheduledMetronome(this);
   }
 
   trigger(time) {
     var attack = this.attack;
     var release = this.release;
+    var period = this.period;
 
     if (period < this.attack + this.release) {
       var scale = period / (this.attack + this.release);
@@ -32,49 +51,38 @@ class Metronome {
       release *= scale;
     }
 
-  this.__envNode.gain.setValueAtTime(0.0, time);
-  this.__envNode.gain.linearRampToValueAtTime(1.0, time + attack);
-  this.__envNode.gain.exponentialRampToValueAtTime(0.0000001, time + attack + release);
-}
+    this.__envNode.gain.setValueAtTime(0.0, time);
+    this.__envNode.gain.linearRampToValueAtTime(1.0, time + attack);
+    this.__envNode.gain.exponentialRampToValueAtTime(0.0000001, time + attack + release);
 
-resetAndReturnNextTime(time) {
-  this.__nextEventTime = time;
-  return time;
-}
+    return time + period;
+  }
 
-makeEventAndReturnNextTime() {
-  this.trigger(this.__nextEventTime);
-  this.__nextEventTime = this.__nextEventTime + this.period;
-  return this.__nextEventTime;
-}
+  set gain(value) {
+    this.__gainNode.gain.value = value;
+  }
 
-set gain(value) {
-  this.__gainNode.gain.value = value;
-  //console.log(gain);
-}
+  get gain() {
+    return this.__gainNode.gain.value;
+  }
 
-get gain() {
-  return this.__gainNode.gain.value;
-}
+  set frequency(value) {
+    this.__osc.frequency.value = value;
+  }
 
-set frequency(value) {
-  this.__osc.frequency.value = value;
-  //console.log(gain);
-}
+  get frequency() {
+    return this.__osc.frequency.value;
+  }
 
-get frequency() {
-  return this.__osc.frequency.value;
-}
+  connect(target) {
+    this.__gainNode.connect(target);
+    return this;
+  }
 
-connect(target) {
-  this.__gainNode.connect(target);
-  return this;
-}
-
-disconnect(output) {
-  this.__gainNode.disconnect(output);
-  return this;
-}
+  disconnect(output) {
+    this.__gainNode.disconnect(output);
+    return this;
+  }
 }
 
 module.exports = Metronome;
