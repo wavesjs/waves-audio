985054daf622d75a0a61866e24d9c96b384131d7
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-08-27 19:27:44 +0200

    - revised es6 version

--- src/index.js
+++ src/index.js
@@ -1,177 +1,139 @@
+/* written in ECMAscript 6 */
 /**
- * @fileoverview WAVE audio library element: a web audio granular engine.
- * @author Karim.Barkati@ircam.fr, Norbert.Schnell@ircam.fr, Victor.Saiz@ircam.fr
- * @version 2.1.1
+ * @fileoverview WAVE audio granular engine
+ * @author Norbert.Schnell@ircam.fr, Victor.Saiz@ircam.fr, Karim.Barkati@ircam.fr
  */
-
 "use strict";
 
-var Scheduled = require("../scheduled");
-var Transported = require("../transported");
+var audioContext = require("../audio-context");
+var EventEngine = require("../event-engine");
 
-class ScheduledGranular extends Scheduled {
-  reset(time) {
-    if (parent.buffer === null)
-      return Infinity; // do not schedule
+class GranularEngine extends EventEngine {
 
-    return time;
-  }
-
-  execute(time) {
-    return parent.trigger(time) + period;
-  }
-}
-
-class GranularEngine {
-
-  constructor(buffer = null, name = "unnamed") {
-    this.name = name;
+  constructor(buffer = null) {
+    super(false); // by default events don't sync to transport position
 
     this.buffer = buffer; // audio buffer
     this.periodAbs = 0.01; // absolute period
-    this.periodRel = 0.0; // period relative to duration
-    this.periodVar = 0.0; // period variation relative to grain period
-    this.position = 0.0; // grain position (onset time) in sec
+    this.periodRel = 0; // period relative to duration
+    this.periodVar = 0; // period variation relative to grain period
+    this.position = 0; // grain position (onset time) in sec
     this.positionVar = 0.003; // grain position variation in sec
     this.durationAbs = 0.1; // absolute grain duration
-    this.durationRel = 0.0; // duration relative to absolute period
-    this.attackAbs = 0.0; // absolute attack time
+    this.durationRel = 0; // duration relative to absolute period
+    this.attackAbs = 0; // absolute attack time
     this.attackRel = 0.5; // attack time relative to duration
-    this.releaseAbs = 0.0; // absolute release time
+    this.releaseAbs = 0; // absolute release time
     this.releaseRel = 0.5; // release time relative to duration
     this.resampling = 0; // resampling in cent
     this.resamplingVar = 0; // resampling variation in cent
     this.centered = true; // whether grain position refers to teh center of teh grain (or the beginning)
 
-    // private properties
-    this.callback = null;
-    this.gainNode = audioContext.createGain();
+    this.__phase = 0;
+    this.__aligned = true;
 
-    // interfaces
-    this.scheduled = new ScheduledGranular(this);
-    this.transported = new Transported(this); // transported but not scheduled
+    this.__gainNode = audioContext.createGain();
+
+    this.outputNode = this.__gainNode;
+  }
+
+  // EventEngine executeEvent
+  executeEvent(time, audioTime) {
+    return this.trigger(audioTime);
   }
 
   set gain(value) {
-    this.gainNode.gain.value = val;
+    this.__gainNode.gain.value = value;
   }
 
   get gain() {
-    return this.gainNode.gain.value;
+    return this.__gainNode.gain.value;
   }
 
-  /**
-   * Audio node connect method.
-   * @public
-   * @chainable
-   */
-  connect(target) {
-    this.gainNode.connect(target || audioContext.destination);
-    return this;
-  }
-
-  /**
-   * Audio node disconnect method.
-   * @public
-   * @chainable
-   */
-  disconnect(index) {
-    this.gainNode.disconnect(index);
-    return this;
-  }
-
-  trigger(grainTime = audioContext.currentTime) { // TODO: check if smart
+  trigger(time) {
+    var grainTime = time || audioContext.currentTime;
     var grainPeriod = this.periodAbs;
     var grainPosition = this.position;
     var grainDuration = this.durationAbs;
-    var resamplingRate = 1.0;
 
-    // calculate resampling
-    if (this.resampling !== 0 || this.resamplingVar > 0) {
-      var randomResampling = (Math.random() - 0.5) * 2.0 * this.resamplingVar;
-      resamplingRate = Math.pow(2.0, (this.resampling + randomResampling) / 1200.0);
-    }
+    if (this.buffer) {
+      var resamplingRate = 1.0;
 
-    grainPeriod += this.periodRel * grainDuration;
-    grainDuration += this.durationRel * grainPeriod;
-
-    // grain period randon variation
-    if (this.periodVar > 0.0)
-      grainPeriod += 2.0 * (Math.random() - 0.5) * this.periodVar * grainPeriod;
-
-    // add transport position
-    grainPosition += this.transported.position;
-
-    if (this.callback) {
-      var params = {
-        position: grainPosition,
-        period: grainPeriod,
-        duration: grainDuration
-      };
-
-      this.callback(params);
-
-      grainPosition = params.position;
-      grainPeriod = params.period;
-      grainDuration = params.duration;
-    }
-
-    // center grain
-    if (this.centered)
-      grainPosition -= 0.5 * grainDuration;
-
-    // randomize grain position
-    if (this.positionVar > 0)
-      grainPosition += (2.0 * Math.random() - 1) * this.positionVar;
-
-    // shorten duration of grains over the edges of the buffer
-    if (grainPosition < 0) {
-      grainDuration += grainPosition;
-      grainPosition = 0;
-    }
-
-    if (grainPosition + grainDuration > this.buffer.duration)
-      grainDuration = this.buffer.duration - grainPosition;
-
-    // make grain
-    if (this.gain > 0 && grainDuration > 0) {
-      // make grain envelope
-      var envelopeNode = audioContext.createGain();
-      var attack = this.attackAbs + this.attackRel * grainDuration;
-      var release = this.releaseAbs + this.releaseRel * grainDuration;
-
-      if (attack + release > grainDuration) {
-        var factor = grainDuration / (attack + release);
-        attack *= factor;
-        release *= factor;
+      // calculate resampling
+      if (this.resampling !== 0 || this.resamplingVar > 0) {
+        var randomResampling = (Math.random() - 0.5) * 2.0 * this.resamplingVar;
+        resamplingRate = Math.pow(2.0, (this.resampling + randomResampling) / 1200.0);
       }
 
-      if (grainTime < audioContext.currentTime)
-        grainTime = audioContext.currentTime;
+      grainPeriod += this.periodRel * grainDuration;
+      grainDuration += this.durationRel * grainPeriod;
 
-      var attackEndTime = grainTime + attack;
-      var grainEndTime = grainTime + grainDuration;
-      var releaseStartTime = grainEndTime - release;
+      // grain period randon variation
+      if (this.periodVar > 0.0)
+        grainPeriod += 2.0 * (Math.random() - 0.5) * this.periodVar * grainPeriod;
 
-      envelopeNode.gain.setValueAtTime(0.0, grainTime);
-      envelopeNode.gain.linearRampToValueAtTime(1.0, attackEndTime);
+      // get transport position
+      if (this.transport)
+        grainPosition = this.transport.position;
 
-      if (releaseStartTime > attackEndTime)
-        envelopeNode.gain.setValueAtTime(1.0, releaseStartTime);
+      // center grain
+      if (this.centered)
+        grainPosition -= 0.5 * grainDuration;
 
-      envelopeNode.gain.linearRampToValueAtTime(0.0, grainEndTime);
-      envelopeNode.connect(this.gainNode);
+      // randomize grain position
+      if (this.positionVar > 0)
+        grainPosition += (2.0 * Math.random() - 1) * this.positionVar;
 
-      // make source
-      var source = audioContext.createBufferSource();
+      // shorten duration of grains over the edges of the buffer
+      if (grainPosition < 0) {
+        grainTime -= grainPosition;
+        grainDuration += grainPosition;
+        grainPosition = 0;
+      }
 
-      source.buffer = this.buffer;
-      source.playbackRate.value = resamplingRate;
-      source.connect(envelopeNode);
-      envelopeNode.connect(this.gainNode);
+      if (grainPosition + grainDuration > this.buffer.duration)
+        grainDuration = this.buffer.duration - grainPosition;
 
-      source.start(grainTime, grainPosition);
-      source.stop(grainTime + grainDuration / resamplingRate);
+      // make grain
+      if (this.gain > 0 && grainDuration > 0) {
+        // make grain envelope
+        var envelopeNode = audioContext.createGain();
+        var attack = this.attackAbs + this.attackRel * grainDuration;
+        var release = this.releaseAbs + this.releaseRel * grainDuration;
+
+        if (attack + release > grainDuration) {
+          var factor = grainDuration / (attack + release);
+          attack *= factor;
+          release *= factor;
+        }
+
+        if (grainTime < audioContext.currentTime)
+          grainTime = audioContext.currentTime;
+
+        var attackEndTime = grainTime + attack;
+        var grainEndTime = grainTime + grainDuration;
+        var releaseStartTime = grainEndTime - release;
+
+        envelopeNode.gain.setValueAtTime(0.0, grainTime);
+        envelopeNode.gain.linearRampToValueAtTime(1.0, attackEndTime);
+
+        if (releaseStartTime > attackEndTime)
+          envelopeNode.gain.setValueAtTime(1.0, releaseStartTime);
+
+        envelopeNode.gain.linearRampToValueAtTime(0.0, grainEndTime);
+        envelopeNode.connect(this.__gainNode);
+
+        // make source
+        var source = audioContext.createBufferSource();
+
+        source.buffer = this.buffer;
+        source.playbackRate.value = resamplingRate;
+        source.connect(envelopeNode);
+        envelopeNode.connect(this.__gainNode);
+
+        source.start(grainTime, grainPosition);
+        source.stop(grainTime + grainDuration / resamplingRate);
+      }
     }
 
     return grainPeriod;
