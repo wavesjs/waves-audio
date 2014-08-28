f5bb87e8eb1c583a848a458c8c9533d203ff1f68
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-08-28 12:52:39 +0200

    - added webkitAudioContext

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
 // instantiates an audio context in the global scope if not there already
-var context = window.audioContext || new AudioContext();
+var context = window.audioContext || new AudioContext() || new webkitAudioContext();
 window.audioContext = context;
 module.exports = context;
