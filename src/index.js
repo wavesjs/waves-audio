5eee349d7e442ef5cae5e009951a9cb702868bbe
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-09-03 17:58:41 +0200

    - added exceptions  - added documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* written in ECMAscript 6 */
 /**
- * @fileoverview WAVE audio global event scheduler based on audio time
+ * @fileoverview WAVE audio event scheduler singleton based on audio time
  * @author Norbert.Schnell@ircam.fr, Victor.Saiz@ircam.fr, Karim.Barkati@ircam.fr
  * @version 5.1.0
  */
@@ -18,13 +18,22 @@ class Scheduler {
     this.__nextTime = Infinity;
     this.__timeout = null;
 
+    /**
+     * scheduler (setTimeout) period
+     * @type {Number}
+     */
     this.period = 0.025;
-    this.advance = 0.1; // how far ahead to schedule events (> period)
+
+    /**
+     * scheduler lookahead time (> period)
+     * @type {Number}
+     */
+    this.lookahead = 0.1;
   }
 
   // global setTimeout scheduling loop
   __tick() {
-    while (this.__nextTime <= audioContext.currentTime + this.advance) {
+    while (this.__nextTime <= audioContext.currentTime + this.lookahead) {
       this.__currentTime = this.__nextTime;
       this.__nextTime = this.__eventQueue.advance(this.__nextTime, this.__nextTime);
     }
@@ -49,14 +58,19 @@ class Scheduler {
   }
 
   /**
-   * Get global scheduler time
+   * Get scheduler time
+   * @return {Number} current scheduler time including lookahead
    */
   get time() {
-    return this.__currentTime || audioContext.currentTime + this.advance;
+    return this.__currentTime || audioContext.currentTime + this.lookahead;
   }
 
   /**
-   * Add a callback to the global scheduler
+   * Add a callback to the scheduler
+   * @param {Function} callback function(time, audioTime) to be called
+   * @param {Number} period callback period
+   * @param {Number} delay of first callback
+   * @return {Object} scheduled object that can be used to call remove and reschedule
    */
   callback(callback, delay = 0) {
     var object = {
@@ -73,7 +87,11 @@ class Scheduler {
   }
 
   /**
-   * Add a repeated callback to the global scheduler
+   * Add a periodically repeated callback to the scheduler
+   * @param {Function} callback function(time, audioTime) to be called periodically
+   * @param {Number} period callback period
+   * @param {Number} delay of first callback
+   * @return {Object} scheduled object that can be used to call remove and reschedule
    */
   repeat(callback, period = 1, delay = 0) {
     var object = {
@@ -91,48 +109,65 @@ class Scheduler {
   }
 
   /**
-   * Add event engine to the global scheduler
+   * Add an event engine to the scheduler
+   * @param {Object} engine event engine to be added to the scheduler
+   * @param {Number} delay scheduling delay time
    */
   add(engine, delay = 0) {
-    if (engine.scheduler === null) {
-      engine.scheduler = this;
-      this.__nextTime = this.__eventQueue.insert(engine, this.time + delay);
-      this.__reschedule();
-    }
+    if (engine.scheduler !== null)
+      throw "object has already been added to a scheduler";
+
+    if (!engine.syncEvent)
+      throw "object does not have a syncEvent method";
+
+    if (!engine.executeEvent)
+      throw "object does not have a executeEvent method";
+
+    engine.scheduler = this;
+    this.__nextTime = this.__eventQueue.insert(engine, this.time + delay);
+    this.__reschedule();
   }
 
   /**
-   * Remove event (engine) from the global scheduler
+   * Remove event engine from the scheduler
+   * @param {Object} engine event engine or callback to be removed from the scheduler
    */
   remove(engine) {
-    if (engine.scheduler === this) {
-      engine.scheduler = null;
-      this.__nextTime = this.__eventQueue.remove(engine);
-      this.__reschedule();
-    }
+    if (engine.scheduler !== this)
+      throw "object has not been added to this scheduler";
+
+    engine.scheduler = null;
+    this.__nextTime = this.__eventQueue.remove(engine);
+    this.__reschedule();
   }
 
   /**
-   * Resychronize event engine
-   * (called by event engine)
+   * Resychronize a scheduled event engine
+   * @param {Object} engine event engine to be resynchronized
    */
   resync(engine) {
-    if (engine.scheduler === this) {
-      this.__nextTime = this.__eventQueue.move(engine, this.time);
-      this.__reschedule();
-    }
+    if (engine.scheduler !== this)
+      throw "object has not been added to this scheduler";
+
+    if (!engine.syncEvent)
+      throw "object does not have a syncEvent method";
+
+    this.__nextTime = this.__eventQueue.move(engine, this.time);
+    this.__reschedule();
   }
 
   /**
-   * Reschedule event engine
-   * (called by event engine)
+   * Reschedule a scheduled event engine or callback
+   * @param {Object} engine event engine or callback to be rescheduled
+   * @param {Number} time time when to reschedule
    */
   reschedule(engine, time) {
-    if (engine.scheduler === this) {
-      this.__nextTime = this.__eventQueue.move(engine, time, false);
-      this.__reschedule();
-    }
+    if (engine.scheduler !== this)
+      throw "object has not been added to this scheduler";
+
+    this.__nextTime = this.__eventQueue.move(engine, time, false);
+    this.__reschedule();
   }
 }
 
-module.exports = new Scheduler; // export global scheduler singleton
+module.exports = new Scheduler; // export scheduler singleton
