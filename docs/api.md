e2df97d01a88a002ee8e6db121cb10a1da4133a5
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-09-23 00:59:40 +0200

    - first satisfying API proposal (version 0.9)

--- docs/api.md
+++ docs/api.md
@@ -6,135 +6,118 @@ written in ECMAscript 6
 
 Author: Norbert.Schnell@ircam.fr, Victor.Saiz@ircam.fr, Karim.Barkati@ircam.fr
 
-## __timeMaster
+## master
 
-Time master to which the time engine is synchronized
+Master (scheduler, transport, player) to which the time engine is synchronized
 
-## getMasterTime()
+## interface
 
-Function provided by time master to get the master time
+Interface used by teh current master
 
-## resetEngineTime(time)
+## transportStartPosition
 
-Function provided by the time master to reset the engine's next time
-
-### Params: 
-
-* **Number** *time* new engine time (immediately if not specified)
-
-## __positionMaster
-
-Position master to which the time engine is synchronized
-
-## __startPosition
-
-Start position of the engine
-
-## getMasterPosition()
-
-Function provided by position master to get the master position
-
-## resyncEnginePosition(time)
-
-Function provided by the position master to request resynchronizing the engine's position
-
-### Params: 
-
-* **Number** *time* new engine time (immediately if not specified)
+Transport start position of the engine (handled by )
 
 ## outputNode
 
 Output audio node
 
-## initTime(time)
+## implementsScheduled
 
-Synchronize engine to master time ("time-based" interface, optional function)
+Check whether the time engine implements the scheduled interface
+
+## implementsTransported
+
+Check whether the time engine implements the transported interface
+
+## implementsSpeedControlled
+
+Check whether the time engine implements the speed-controlled interface
+
+## currentTime
+
+Get the time engine's current master time
+
+## currentPosition
+
+Get the time engine's current master position
+
+Advance engine time (scheduled interface)
 
 ### Params: 
 
-* **Number** *time* current master time (based on audio time)
-
-### Return:
-
-* **Number** first time 
-This optional function is called by the time master and allows the engine to
-return its first time.
-If the engine returns Infinity or -Infinity, it is not called again until it is
-reset by the time master or it calls resetEngineTime().
-
-Advance engine time ("time-based" interface)
-
-### Params: 
-
-* **Number** *time* current master time (based on audio time)
+* **Number** *time* current scheduler time (based on audio time)
 
 ### Return:
 
 * **Number** next engine time 
-This function is called by the time master to let the engine do its work
-synchronized to the master's time.
+This function is called by the scheduler to let the engine do its work
+synchronized to the scheduler time.
 If the engine returns Infinity, it is not called again until it is restarted by
-the time master or it calls resyncEnginePosition() with a valid position.
+the scheduler or it calls resyncEnginePosition() with a valid position.
 
-Synchronize engine to master position ("position-based" interface)
+## resetEngineTime(time)
+
+Function provided by the scheduler to reset the engine's next time
 
 ### Params: 
 
-* **Number** *position* current master position to synchronize to
-* **Number** *time* current master time (based on audio time)
+* **Number** *time* new engine time (immediately if not specified)
+
+Synchronize engine to transport position (transported interface)
+
+### Params: 
+
+* **Number** *position* current transport position to synchronize to
+* **Number** *time* current scheduler time (based on audio time)
 * **Number** *speed* current speed
 
 ### Return:
 
 * **Number** next position (given the playing direction) 
 This function is called by the msater and allows the engine for synchronizing
-(seeking) to the current master position and to return its next position.
+(seeking) to the current transport position and to return its next position.
 If the engine returns Infinity or -Infinity, it is not called again until it is
-resynchronized by the position master or it calls resyncEnginePosition().
+resynchronized by the transport or it calls resyncEnginePosition().
 
-Advance engine position ("position-based" interface)
+Advance engine position (transported interface)
 
 ### Params: 
 
-* **Number** *time* current master time (based on audio time)
-* **Number** *position* current master position
+* **Number** *time* current scheduler time (based on audio time)
+* **Number** *position* current transport position
 * **Number** *speed* current speed
 
 ### Return:
 
 * **Number** next engine position (given the playing direction) 
-This function is called by the position master to let the engine do its work
-aligned to the master's position.
+This function is called by the transport to let the engine do its work
+aligned to the transport's position.
 If the engine returns Infinity or -Infinity, it is not called again until it is
-resynchronized by the position master or it calls resyncEnginePosition().
+resynchronized by the transport or it calls resyncEnginePosition().
 
-Set engine speed ("speed-based" interface)
+## resyncEnginePosition(time)
+
+Function provided by the transport to request resynchronizing the engine's position
 
 ### Params: 
 
-* **Number** *speed* current master speed 
-This function is called by the speed master to propagate the master's speed to the engine.
+* **Number** *time* new engine time (immediately if not specified)
+
+Set engine speed (speed-controlled interface)
+
+### Params: 
+
+* **Number** *time* current scheduler time (based on audio time)
+* **Number** *position* current transport position
+* **Number** *speed* current transport speed 
+This function is called by the transport to propagate the transport speed to the engine.
 The speed can be of any bewteen -16 and 16.
 With a speed of 0 the engine is halted.
 
-Seek engine to a given position ("speed-based" interface)
+## removeFromMaster()
 
-### Params: 
-
-* **Number** *position* position to seek to 
-This function is called by the speed master to propagate position jumps to the engine.
-
-## implementsTimeBased
-
-Return whether the time engine implements the time-based interface
-
-## implementsPositionBased
-
-Return whether the time engine implements the position-based interface
-
-## implementsSpeedBased
-
-Return whether the time engine implements the speed-based interface
+Remove engine from current master
 
 ## connect(target)
 
--- src/index.js
+++ src/index.js
@@ -5,53 +5,27 @@
  */
 "use strict";
 
+var audioContext = require("../audio-context");
+
 class TimeEngine {
   constructor() {
     /**
-     * Time master to which the time engine is synchronized
+     * Master (scheduler, transport, player) to which the time engine is synchronized
      * @type {Object}
      */
-    this.__timeMaster = null;
+    this.master = null;
 
     /**
-     * Function provided by time master to get the master time
-     * @type {Function}
+     * Interface used by teh current master
+     * @type {String}
      */
-    this.getMasterTime = function() {
-      return 0;
-    };
+    this.interface = null;
 
     /**
-     * Function provided by the time master to reset the engine's next time
-     * @param {Number} time new engine time (immediately if not specified)
-     */
-    this.resetEngineTime = function(time) {};
-
-    /**
-     * Position master to which the time engine is synchronized
+     * Transport start position of the engine (handled by )
      * @type {Object}
      */
-    this.__positionMaster = null;
-
-    /**
-     * Start position of the engine
-     * @type {Object}
-     */
-    this.__startPosition = 0;
-
-    /**
-     * Function provided by position master to get the master position
-     * @type {Function}
-     */
-    this.getMasterPosition = function() {
-      return 0;
-    };
-
-    /**
-     * Function provided by the position master to request resynchronizing the engine's position
-     * @param {Number} time new engine time (immediately if not specified)
-     */
-    this.resyncEnginePosition = function() {};
+    this.transportStartPosition = 0;
 
     /**
      * Output audio node
@@ -60,113 +34,17 @@ class TimeEngine {
     this.outputNode = null;
   }
 
-  set timeMaster(timeMaster) {
-    this.__timeMaster = timeMaster;
-  }
-
-  get timeMaster() {
-    return this.__timeMaster;
-  }
-
-  set positionMaster(positionMaster) {
-    this.__positionMaster = positionMaster;
-  }
-
-  get positionMaster() {
-    return this.__positionMaster;
-  }
-
   /**
-   * Synchronize engine to master time ("time-based" interface, optional function)
-   * @param {Number} time current master time (based on audio time)
-   * @return {Number} first time
-   *
-   * This optional function is called by the time master and allows the engine to
-   * return its first time.
-   * If the engine returns Infinity or -Infinity, it is not called again until it is
-   * reset by the time master or it calls resetEngineTime().
-   */
-  initTime(time) {
-    return time;
-  }
-
-  /**
-   * Advance engine time ("time-based" interface)
-   * @param {Number} time current master time (based on audio time)
-   * @return {Number} next engine time
-   *
-   * This function is called by the time master to let the engine do its work
-   * synchronized to the master's time.
-   * If the engine returns Infinity, it is not called again until it is restarted by
-   * the time master or it calls resyncEnginePosition() with a valid position.
-   */
-  // advanceTime(time) {
-  //   return time;
-  // }
-
-  /**
-   * Synchronize engine to master position ("position-based" interface)
-   * @param {Number} position current master position to synchronize to
-   * @param {Number} time current master time (based on audio time)
-   * @param {Number} speed current speed
-   * @return {Number} next position (given the playing direction)
-   *
-   * This function is called by the msater and allows the engine for synchronizing
-   * (seeking) to the current master position and to return its next position.
-   * If the engine returns Infinity or -Infinity, it is not called again until it is
-   * resynchronized by the position master or it calls resyncEnginePosition().
-   */
-  // syncPosition(time, position, speed) {
-  //   return position;
-  // }
-
-  /**
-   * Advance engine position ("position-based" interface)
-   * @param {Number} time current master time (based on audio time)
-   * @param {Number} position current master position
-   * @param {Number} speed current speed
-   * @return {Number} next engine position (given the playing direction)
-   *
-   * This function is called by the position master to let the engine do its work
-   * aligned to the master's position.
-   * If the engine returns Infinity or -Infinity, it is not called again until it is
-   * resynchronized by the position master or it calls resyncEnginePosition().
-   */
-  // advancePosition(time, position, speed) {
-  //   return position;
-  // }
-
-  /**
-   * Set engine speed ("speed-based" interface)
-   * @param {Number} speed current master speed
-   *
-   * This function is called by the speed master to propagate the master's speed to the engine.
-   * The speed can be of any bewteen -16 and 16.
-   * With a speed of 0 the engine is halted.
-   */
-  // set speed(speed) {
-  // }
-
-  /**
-   * Seek engine to a given position ("speed-based" interface)
-   * @param {Number} position position to seek to
-   *
-   * This function is called by the speed master to propagate position jumps to the engine.
-   */
-  // seek(speed) {
-  // }
-
-  /**
-   * Return whether the time engine implements the time-based interface
+   * Check whether the time engine implements the scheduled interface
    **/
-  get implementsTimeBased() {
+  get implementsScheduled() {
     return (this.advanceTime && this.advanceTime instanceof Function);
   }
 
   /**
-   * Return whether the time engine implements the position-based interface
+   * Check whether the time engine implements the transported interface
    **/
-  get implementsPositionBased() {
+  get implementsTransported() {
     return (
       this.syncPosition && this.syncPosition instanceof Function &&
       this.advancePosition && this.advancePosition instanceof Function
@@ -174,13 +52,187 @@ class TimeEngine {
   }
 
   /**
-   * Return whether the time engine implements the speed-based interface
+   * Check whether the time engine implements the speed-controlled interface
    **/
-  get implementsSpeedBased() {
-    return (
-      Object.getOwnPropertyDescriptor(Object.getPrototypeOf(this), "speed") &&
-      this.seek && this.seek instanceof Function
-    );
+  get implementsSpeedControlled() {
+    return (this.syncSpeed && this.syncSpeed instanceof Function);
+  }
+
+  /**
+   * Get the time engine's current master time
+   * @type {Function}
+   *
+   * This function provided by the master.
+   */
+  get currentTime() {
+    return audioContext.currentTime;
+  }
+
+  /**
+   * Get the time engine's current master position
+   * @type {Function}
+   *
+   * This function provided by the master.
+   */
+  get currentPosition() {
+    return 0;
+  };
+
+  /**
+   * Advance engine time (scheduled interface)
+   * @param {Number} time current scheduler time (based on audio time)
+   * @return {Number} next engine time
+   *
+   * This function is called by the scheduler to let the engine do its work
+   * synchronized to the scheduler time.
+   * If the engine returns Infinity, it is not called again until it is restarted by
+   * the scheduler or it calls resyncEnginePosition() with a valid position.
+   */
+  // advanceTime(time) {
+  //   return time;
+  // }
+
+  /**
+   * Function provided by the scheduler to reset the engine's next time
+   * @param {Number} time new engine time (immediately if not specified)
+   */
+  resetEngineTime(time) {}
+
+  /**
+   * Synchronize engine to transport position (transported interface)
+   * @param {Number} position current transport position to synchronize to
+   * @param {Number} time current scheduler time (based on audio time)
+   * @param {Number} speed current speed
+   * @return {Number} next position (given the playing direction)
+   *
+   * This function is called by the msater and allows the engine for synchronizing
+   * (seeking) to the current transport position and to return its next position.
+   * If the engine returns Infinity or -Infinity, it is not called again until it is
+   * resynchronized by the transport or it calls resyncEnginePosition().
+   */
+  // syncPosition(time, position, speed) {
+  //   return position;
+  // }
+
+  /**
+   * Advance engine position (transported interface)
+   * @param {Number} time current scheduler time (based on audio time)
+   * @param {Number} position current transport position
+   * @param {Number} speed current speed
+   * @return {Number} next engine position (given the playing direction)
+   *
+   * This function is called by the transport to let the engine do its work
+   * aligned to the transport's position.
+   * If the engine returns Infinity or -Infinity, it is not called again until it is
+   * resynchronized by the transport or it calls resyncEnginePosition().
+   */
+  // advancePosition(time, position, speed) {
+  //   return position;
+  // }
+
+  /**
+   * Function provided by the transport to request resynchronizing the engine's position
+   * @param {Number} time new engine time (immediately if not specified)
+   */
+  resyncEnginePosition() {};
+
+  /**
+   * Set engine speed (speed-controlled interface)
+   * @param {Number} time current scheduler time (based on audio time)
+   * @param {Number} position current transport position
+   * @param {Number} speed current transport speed
+   *
+   * This function is called by the transport to propagate the transport speed to the engine.
+   * The speed can be of any bewteen -16 and 16.
+   * With a speed of 0 the engine is halted.
+   */
+  // syncSpeed(time, position, speed) {
+  // }
+
+  __setGetters(getCurrentTime, getCurrentPosition) {
+    if (getCurrentTime) {
+      Object.defineProperty(this, 'currentTime', {
+        configurable: true,
+        get: getCurrentTime,
+        set: function(time) {}
+      });
+    }
+
+    if (getCurrentPosition) {
+      Object.defineProperty(this, 'currentPosition', {
+        configurable: true,
+        get: getCurrentPosition,
+        set: function(position) {}
+      });
+    }
+  }
+
+  __deleteGetters() {
+    delete this.currentTime;
+    delete this.currentPosition;
+  }
+
+  setScheduled(scheduler, resetEngineTime, getCurrentTime, getCurrentPosition) {
+    this.master = scheduler;
+    this.interface = "scheduled";
+
+    this.__setGetters(getCurrentTime, getCurrentPosition);
+
+    if (resetEngineTime)
+      this.resetEngineTime = resetEngineTime;
+  }
+
+  resetScheduled() {
+    this.__deleteGetters();
+
+    delete this.resetEngineTime;
+
+    this.master = null;
+    this.interface = null;
+  }
+
+  setTransported(transport, startPosition, resyncEnginePosition, getCurrentTime, getCurrentPosition) {
+    this.master = transport;
+    this.interface = "transported";
+
+    this.transportStartPosition = startPosition;
+
+    this.__setGetters(getCurrentTime, getCurrentPosition);
+
+    if (resyncEnginePosition)
+      this.resyncEnginePosition = resyncEnginePosition;
+  }
+
+  resetTransported() {
+    this.__deleteGetters();
+
+    delete this.resyncEnginePosition;
+
+    this.transportStartPosition = 0;
+    this.master = null;
+    this.interface = null;
+  }
+
+  setSpeedControlled(master, getCurrentTime, getCurrentPosition) {
+    this.master = master;
+    this.interface = "speed-controlled";
+
+    this.__setGetters(getCurrentTime, getCurrentPosition);
+  }
+
+  resetSpeedControlled() {
+    this.__deleteGetters();
+
+    this.master = null;
+    this.interface = null;
+  }
+
+  /**
+   * Remove engine from current master
+   */
+  removeFromMaster() {
+    if (this.master)
+      this.master.remove(this);
   }
 
   /**
