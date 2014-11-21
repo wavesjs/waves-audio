e33db92e6ad04833c0d54353c4548fa21dcd8c8f
vectorsize <vectorsize@gmail.com>
2014-11-21 17:55:37 +0100

    inludes monkey patch - simplified version - singleton style

--- src/index.js
+++
@@ -1,5 +0,0 @@
-
-// instantiates an audio context in the global scope if not there already
-var context = window.audioContext || new AudioContext() || new webkitAudioContext();
-window.audioContext = context;
-module.exports = context;
