57636bed91419bfa6836d2925fed9d35ad96f8ae
vectorsize <vectorsize@gmail.com>
2014-11-21 17:52:38 +0100

    rm node_modules from git; update gulfile;

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var packageJson = require('./package.json');
 
-require('./node_modules/module-boilerplate/load-dependencies')(gulp, packageJson);
+require('module-boilerplate/load-dependencies')(gulp, packageJson);
 
-tasks = require('./node_modules/module-boilerplate/tasks.json');
+tasks = require('module-boilerplate/tasks.json');
 
 for(var i in tasks) {
-  require('./node_modules/module-boilerplate/tasks/' + tasks[i])(gulp, packageJson);
+  require('module-boilerplate/tasks/' + tasks[i])(gulp, packageJson);
 }
+
