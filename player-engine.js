c5a3d766dd477b6bc0eb57e53c0d401848e54be1
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-12-07 01:10:17 +0100

    - re-built with latest "gulp build"

--- player-engine.js
+++ player-engine.js
@@ -8,7 +8,7 @@
 var audioContext = require("audio-context");
 var TimeEngine = require("time-engine");
 
-var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN$0 = function(t,s){for(var p in s){if(s.hasOwnProperty(p)){DP$0(t,p,Object.getOwnPropertyDescriptor(s,p));}}return t};MIXIN$0(PlayerEngine, super$0);
+var PlayerEngine = (function(super$0){var PRS$0 = (function(o,t){o["__proto__"]={"a":t};return o["a"]===t})({},{});var DP$0 = Object.defineProperty;var GOPD$0 = Object.getOwnPropertyDescriptor;var MIXIN$0 = function(t,s){for(var p in s){if(s.hasOwnProperty(p)){DP$0(t,p,GOPD$0(s,p));}}return t};var SP$0 = Object.setPrototypeOf||function(o,p){if(PRS$0){o["__proto__"]=p;}else {DP$0(o,"__proto__",{"value":p,"configurable":true,"enumerable":false,"writable":true});}return o};var OC$0 = Object.create;if(!PRS$0)MIXIN$0(PlayerEngine, super$0);var proto$0={};
   function PlayerEngine() {var buffer = arguments[0];if(buffer === void 0)buffer = null;
     this.transport = null; // set when added to transporter
 
@@ -35,9 +35,9 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
     this.__playingSpeed = 1;
 
     this.outputNode = this.__gainNode = audioContext.createGain();
-  }PlayerEngine.prototype = Object.create(super$0.prototype, {"constructor": {"value": PlayerEngine, "configurable": true, "writable": true}, cyclic: {"get": cyclic$get$0, "set": cyclic$set$0, "configurable": true, "enumerable": true}, gain: {"get": gain$get$0, "set": gain$set$0, "configurable": true, "enumerable": true} });DP$0(PlayerEngine, "prototype", {"configurable": false, "enumerable": false, "writable": false});
+  }if(super$0!==null)SP$0(PlayerEngine,super$0);PlayerEngine.prototype = OC$0(super$0!==null?super$0.prototype:null,{"constructor":{"value":PlayerEngine,"configurable":true,"writable":true}, cyclic: {"get": $cyclic_get$0, "set": $cyclic_set$0, "configurable":true,"enumerable":true}, gain: {"get": $gain_get$0, "set": $gain_set$0, "configurable":true,"enumerable":true}});DP$0(PlayerEngine,"prototype",{"configurable":false,"enumerable":false,"writable":false});
 
-  PlayerEngine.prototype.__start = function(time, position, speed) {
+  proto$0.__start = function(time, position, speed) {
     if (this.buffer) {
       var bufferDuration = this.buffer.duration;
 
@@ -65,9 +65,9 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
         this.__bufferSource.connect(this.__envNode);
       }
     }
-  }
+  };
 
-  PlayerEngine.prototype.__halt = function(time) {
+  proto$0.__halt = function(time) {
     if (this.__bufferSource) {
       this.__envNode.gain.cancelScheduledValues(time);
       this.__envNode.gain.setValueAtTime(this.__envNode.gain.value, time);
@@ -77,10 +77,10 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
       this.__bufferSource = null;
       this.__envNode = null;
     }
-  }
+  };
 
   // TimeEngine method (speed-controlled interface)
-  PlayerEngine.prototype.syncSpeed = function(time, position, speed) {var seek = arguments[3];if(seek === void 0)seek = false;
+  proto$0.syncSpeed = function(time, position, speed) {var seek = arguments[3];if(seek === void 0)seek = false;
     var lastSpeed = this.__speed;
 
     if (speed !== lastSpeed || seek) {
@@ -97,13 +97,13 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
 
       this.__speed = speed;
     }
-  }
+  };
 
   /**
    * Set whether the audio buffer is considered as cyclic
    * @param {Bool} cyclic whether the audio buffer is considered as cyclic
    */
-  function cyclic$set$0(cyclic) {
+  function $cyclic_set$0(cyclic) {
     if (cyclic !== this.__cyclic) {
       var time = this.currentTime;
       var position = this.currentosition;
@@ -120,7 +120,7 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
    * Get whether the audio buffer is considered as cyclic
    * @return {Bool} whether the audio buffer is considered as cyclic
    */
-  function cyclic$get$0() {
+  function $cyclic_get$0() {
     return this.__cyclic;
   }
 
@@ -128,7 +128,7 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
    * Set gain
    * @param {Number} value linear gain factor
    */
-  function gain$set$0(value) {
+  function $gain_set$0(value) {
     var time = this.__sync();
 
     this.__gainNode.cancelScheduledValues(time);
@@ -140,9 +140,9 @@ var PlayerEngine = (function(super$0){var DP$0 = Object.defineProperty;var MIXIN
    * Get gain
    * @return {Number} current gain
    */
-  function gain$get$0() {
+  function $gain_get$0() {
     return this.__gainNode.gain.value;
   }
-;return PlayerEngine;})(TimeEngine);
+MIXIN$0(PlayerEngine.prototype,proto$0);proto$0=void 0;return PlayerEngine;})(TimeEngine);
 
 module.exports = PlayerEngine;
