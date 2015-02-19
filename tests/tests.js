0fab2c9914e742bcf69eaa651abd682896e65c13
ouhouhsami <samuel.goldszmidt@gmail.com>
2015-02-19 15:43:39 +0100

    good first pass

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
 var sinon = require('sinon');
 
+var audioContext = require("audio-context");
 var scheduler = require('../scheduler.es6.js');
 var TimeEngine = require("time-engine");
+
+var currentTimeDeviation = 128 / 44100; // the audioContext.currentTime accuracy
 
 describe("Scheduler", function() {
     afterEach(function() {
@@ -41,8 +44,71 @@ describe("Scheduler", function() {
             return time + 90;
         }
         scheduler.add(engine);
+        assert.equal(scheduler.__engines.length, 1);
         scheduler.remove(engine);
         assert.equal(scheduler.__engines.length, 0);
         assert.equal(scheduler.__queue.head, null);
+    });
+    it('should reset an engine correctly', function(done) {
+        var engine = new TimeEngine();
+        var initialTime = audioContext.currentTime;
+        engine.advanceTime = function(time) {
+            assert.equal(initialTime + 0.5, time)
+            done();
+            return Infinity;
+        }
+        scheduler.add(engine);
+        assert.equal(scheduler.__engines.length, 1);
+        scheduler.reset(engine, initialTime + 0.5);
+        assert.equal(scheduler.__engines.length, 1);
+    });
+    it('should add a callback at a time correctly', function(done) {
+        var initialTime = audioContext.currentTime;
+        var cb = function(time) {
+            assert((initialTime + 1.0 - currentTimeDeviation) < time < (initialTime + 1.0 + currentTimeDeviation));
+            assert.equal(initialTime + 1.0, time)
+            done();
+        };
+        scheduler.callback(cb, initialTime + 1.0);
+    });
+    it('should remove from queue an engine that returns null', function(done) {
+        var engine = new TimeEngine();
+        var initialTime = audioContext.currentTime;
+        engine.advanceTime = function(time) {
+            return null;
+        };
+        scheduler.add(engine, initialTime);
+        assert.equal(scheduler.__engines.length, 1);
+        setTimeout(() => {
+            assert.equal(scheduler.__engines.length, 0);
+            done()
+        }, initialTime + scheduler.period*1000)
+    });
+    it('should unschedule an engine that return Infinity, but keep in as an engine', function(done){
+        var engine = new TimeEngine();
+        var initialTime = audioContext.currentTime;
+        var times = [0.1, Infinity];
+        var i = -1;
+        engine.advanceTime = function(time) {
+            i++
+            return time+times[i];
+        };
+        scheduler.add(engine, initialTime);
+        assert.equal(scheduler.__engines.length, 1);
+        setTimeout(() => {
+            assert.equal(scheduler.__engines.length, 1);
+            done()
+        }, initialTime+0.1+scheduler.period*1000)
+    });
+    it('should clear the scheduler correctly', function(){
+        var engine = new TimeEngine();
+        var initialTime = audioContext.currentTime;
+        engine.advanceTime = function(time) {
+            return time+0.1;
+        };
+        scheduler.add(engine, initialTime);
+        assert.equal(scheduler.__engines.length, 1);
+        scheduler.clear();
+        assert.equal(scheduler.__engines.length, 0);
     })
 })
