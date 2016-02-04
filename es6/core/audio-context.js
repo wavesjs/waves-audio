7ec716b7051b2d55afdbb890bb08fdb8a146b583
Norbert Schnell <Norbert.Schnell@ircam.fr>
2016-02-04 18:52:50 +0100

    removed monkey patch and added iOS sample rate hack

--- es6/core/audio-context.js
+++ es6/core/audio-context.js
@@ -1,10 +1,19 @@
-// monkeypatch old webAudioAPI
-import monkeyPatch from './ac-monkeypatch';
+var AudioContext = window.webkitAudioContext || window.AudioContext;
+var audioContext = null;
 
-// exposes a single instance
-var audioContext;
+if(AudioContext) {
+  audioContext = new AudioContext();
 
-if (window.AudioContext)
-  audioContext = new window.AudioContext();
+  if (/(iPhone|iPad)/i.test(navigator.userAgent) && audioContext.sampleRate !== 44100) {
+    var buffer = audioContext.createBuffer(1, 1, 44100);
+    var dummy = audioContext.createBufferSource();
+    dummy.buffer = buffer;
+    dummy.connect(audioContext.destination);
+    dummy.start(0);
+    dummy.disconnect();
+    audioContext.close();
+    audioContext = new AudioContext();
+  }
+}
 
 export default audioContext;
