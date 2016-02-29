cf36c667a12bad999df3b184a4a9569b9176a3ed
Norbert Schnell <Norbert.Schnell@ircam.fr>
2016-02-29 13:02:18 +0100

    fixed audio context

--- es6/core/audio-context.js
+++ es6/core/audio-context.js
@@ -1,12 +1,12 @@
 // exposes a single instance
 var audioContext = null;
 
-var AudioContext = window.webkitAudioContext || window.AudioContext;
+var AudioContext = window.AudioContext || window.webkitAudioContext;
 
 if(AudioContext) {
   audioContext = new AudioContext();
 
-  if (/(iPhone|iPad)/i.test(navigator.userAgent) && audioContext.sampleRate !== 44100) {
+  if (/(iPhone|iPad)/i.test(navigator.userAgent) && audioContext.sampleRate < 44100) {
     var buffer = audioContext.createBuffer(1, 1, 44100);
     var dummy = audioContext.createBufferSource();
     dummy.buffer = buffer;
